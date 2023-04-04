6e7c87c66f3b377e5ad7c3255d519e0351c0821d
LAPTOP-FQHHCE6S\vishn <vishnudas08@gmail.com>
2023-04-04 21:39:28 +0530

    End of Section4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,34 +1,35 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] // GET /api/Users
-    public class UsersController : ControllerBase
+    [Authorize]
+    public class UsersController : BaseApiController
     {
-         
+
         private readonly DataContext _context;
 
         public UsersController(DataContext context)
         {
             _context = context;
-             
+
         }
+        [AllowAnonymous]
         [HttpGet]
 
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            var users= await _context.Users.ToListAsync();
-            return users;      
+            var users = await _context.Users.ToListAsync();
+            return users;
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsers(int id)
         {
-            return  await _context.Users.FindAsync(id);
-        }  
+            return await _context.Users.FindAsync(id);
+        }
 
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,17 +9,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")) ;
-});
- builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
  app.UseCors(builder => builder.WithOrigins("https://localhost:4200", "http://localhost:4200")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod());
+
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
